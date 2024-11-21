e7c2c78421ea3ef23b4cdb344b299b69c0129c89
SalasainAhmadM <salasainahmad@gmail.com>
2024-11-21 23:08:16 +0800

    with sweetalert

--- components/homepage_nav.php
+++ components/homepage_nav.php
@@ -5,7 +5,8 @@
       <img src="../SmartStoreManager/assets/logo.png" style="height: 50px; margin-right: 10px;">
       Smart Store Manager
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
@@ -14,10 +15,10 @@
           <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house"></i> Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fa-solid fa-right-to-bracket"></i> Login</a>
+          <a class="nav-link" href="#" onclick="showLoginModal()"><i class="fa-solid fa-right-to-bracket"></i> Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fa-solid fa-user-plus"></i> Register</a>
+          <a class="nav-link" href="#" onclick="showRegisterModal()"><i class="fa-solid fa-user-plus"></i> Register</a>
         </li>
       </ul>
     </div>
