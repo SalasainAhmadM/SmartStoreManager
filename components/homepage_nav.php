7891d3425fcd6734e852730ce78545db5349e320
Nadeer Mukaram <nadeermukaram@gmail.com>
2024-11-22 00:46:59 +0800

    new logo

--- components/homepage_nav.php
+++ components/homepage_nav.php
@@ -2,7 +2,7 @@
 <nav class="navbar bg-dark navbar-expand-lg">
   <div class="container">
     <a class="navbar-brand" href="#">
-      <img src="../SmartStoreManager/assets/logo.png" style="height: 50px; margin-right: 10px;">
+      <img src="../SmartStoreManager/assets/logo.png" style="height: 30px; margin-right: 10px;">
       Smart Store Manager
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
