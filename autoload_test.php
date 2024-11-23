97dd614109b25e8f57895939d6d25e6a96471e5f
SalasainAhmadM <salasainahmad@gmail.com>
2024-11-23 11:27:24 +0800

    all goods

--- autoload_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-if (class_exists('Dotenv\Dotenv')) {
-    echo "Dotenv\Dotenv class found!";
-} else {
-    echo "Dotenv\Dotenv class NOT found!";
-}
